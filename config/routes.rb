820988160a6e7a4ba78dac35742b581be408f711
Jonah George <jonah.george@me.com>
2016-08-10 17:27:54 -0700

    Scaffold out posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: redirect("/users")
-  resources :users
+  root to: redirect("/posts")
+  resources :users, only: [:new, :create]
+  resources :posts, except: [:destroy]
 
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,14 @@
 ActiveRecord::Schema.define(version: 20160811235913) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "plpgsql" i
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"

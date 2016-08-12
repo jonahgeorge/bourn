06a933320253e7f53806c685fe03b68c2f36fad5
Jonah George <jonah.george@me.com>
2016-08-12 09:28:26 -0700

    Add tags hack

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,20 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def tag_regex
+    /\[(.*?)\]/
+  end
+
+  def title_without_tags
+    title.gsub(tag_regex, '').strip
+  end
+
+  def tags
+    title.scan(tag_regex).flatten
+  end
+
+  def composite_title
+    html_tags = tags.map { |t| "<span class='tag tag-primary'>#{t}</span>" }.inject("", :+)
+    title_without_tags + "&nbsp;" + html_tags
+  end
 end

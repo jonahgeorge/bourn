dc16548306486d965ec13722d4f7b40f1438e1eb
Jonah George <jonah.george@me.com>
2016-08-16 08:29:10 -0700

    Switch to master branch of Pundit

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 
-gem 'pundit'
+gem 'pundit', github: "elabs/pundit", branch: "master"
 gem 'redcarpet'
 
 # Use jquery as the JavaScript library
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,15 +1,21 @@
 class PostPolicy < ApplicationPolicy
   attr_reader :user, :post
 
+  def initialize(user, post)
+    raise Pundit::NotAuthorizedError, "must be logged in" unless user
+    @user = user
+    @post = post
+  end
+
   def update?
-    post.user_id == user.id
+    @post.user == @user
   end
 
   def edit?
-    post.user_id == user.id
+    @post.user == @user
   end
 
   def destroy?
-    post.user_id == user.id
+    @post.user == @user
   end
 end

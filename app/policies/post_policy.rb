d77f90880e847dea0003b74ac1e8c6e03d1e7058
Jonah George <jonah.george@me.com>
2016-08-16 08:34:46 -0700

    Cleanup policies

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,16 +2,11 @@ class PostPolicy < ApplicationPolicy
   attr_reader :user, :post
 
   def initialize(user, post)
-    raise Pundit::NotAuthorizedError, "must be logged in" unless user
     @user = user
     @post = post
   end
 
   def update?
-    @post.user == @user
-  end
-
-  def edit?
     @post.user == @user
   end
 

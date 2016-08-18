afe683f908451b3ff4101370d17323fcdb2413d0
Jonah George <jonah.george@me.com>
2016-08-17 18:00:05 -0700

    Add data migration

--- db/migrate/20160818002007_add_tags_to_posts.rb
+++ db/migrate/20160818002007_add_tags_to_posts.rb
@@ -2,5 +2,13 @@ class AddTagsToPosts < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :tags, :string, array: true, default: []
     add_index :posts, :tags, using: 'gin'
+
+    tag_regex = /\[(.*?)\]/
+    Post.all.each do |post|
+      tags = post.title.scan(tag_regex).flatten
+      post.title = post.title.gsub(tag_regex, "").strip
+      post.tags = tags
+      post.save
+    end
   end
 end

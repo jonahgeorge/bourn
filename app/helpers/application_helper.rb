183e51a4664e8c7f2df86e1fb0bea2ac9b17d3a8
Jonah George <jonah.george@me.com>
2017-04-27 10:57:02 -0700

    Switch Gravatar link to https

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def avatar_url(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=46"
+    "https://gravatar.com/avatar/#{gravatar_id}.png?s=46"
   end
 end

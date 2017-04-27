0a8480b5201459d4b68dce3084d52841bdcc650e
Jonah George <jonah.george@me.com>
2017-04-27 10:45:58 -0700

    Remove guest image, reverting to gravatar logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def avatar_url(user)
-    default_url = "#{root_url}images/guest.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=46&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=46"
   end
 end

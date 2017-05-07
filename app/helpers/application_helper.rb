d33ccf80f237441e6087fa32d568f784bb5c7fc4
Jonah George <jonah.george@me.com>
2017-05-07 12:44:45 -0700

    [closes #31] Better gravatar icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def avatar_url(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "https://gravatar.com/avatar/#{gravatar_id}.png?s=256"
+    "https://gravatar.com/avatar/#{gravatar_id}.png?size=256&default=identicon&rating=pg"
   end
 
   def link_to_tags(text, pattern)

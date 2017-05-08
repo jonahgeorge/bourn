c74386c932569d112bbaf262af959dd9a4caec29
Jonah George <jonah.george@me.com>
2017-05-07 22:47:46 -0700

    Enable admin interfaces

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -9,6 +9,27 @@ module Admin
     end
 
     def update
+      @post = Post.find(params[:id])
+
+      if @post.update(post_params)
+        redirect_to admin_posts_path, notice: "Successfully updated post."
+      else
+        render :edit
+      end
     end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.children.destroy_all
+      @post.destroy
+
+      redirect_to admin_posts_path, notice: "Successfully deleted post."
+    end
+
+    private
+
+      def post_params
+        params.require(:post).permit(:body, :is_visible)
+      end
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,6 +9,27 @@ module Admin
     end
 
     def update
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        redirect_to admin_users_path, notice: "Successfully updated user."
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.posts.destroy_all
+      @user.destroy
+
+      redirect_to admin_users_path, notice: "Successfully deleted user."
+    end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :role)
     end
   end
 end

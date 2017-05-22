6dd218b1bd161d5cec477037e72486a7a18e1bce
Jonah George <jonah.george@me.com>
2017-05-21 18:50:44 -0700

    wip

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,24 +2,51 @@ class SubscriptionsController < ApplicationController
   skip_before_action :require_subscription
 
   def new
+    @form = SubscriptionForm.new
   end
 
   def create
-  #   @amount_in_cents = 100
-  #
-  #   cu = Stripe::Customer.retrieve(current_user.stripe_id)
-  #   cu.save
-  #
-  #   charge = Stripe::Charge.create(
-  #     :customer    => current_user.stripe_id,
-  #     :amount      => @amount_in_cents,
-  #     :description => 'Rails Stripe customer',
-  #     :currency    => 'usd'
-  #   )
-  #
-  #   redirect_to root_path
-  # rescue Stripe::CardError => e
-  #   flash[:error] = e.message
-  #   redirect_to new_subscription_path
+    @form = SubscriptionForm.new(subscription_form_params)
+
+    if @form.valid?
+      customer = Stripe::Customer.retrieve(current_user.stripe_id)
+      customer.sources.create(source_params)
+      # customer.save
+
+      charge = process_charge(current_user.stripe_id)
+      redirect_to root_path
+    else
+      render :new
+    end
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_subscription_path
+  end
+
+  private
+
+  def subscription_form_params
+    params.require(:subscription_form).permit(:number, :exp_month, :exp_year, :cvc, :postal_code)
+  end
+
+  def process_charge(stripe_id)
+    Stripe::Charge.create(
+      :customer    => stripe_id,
+      :amount      => 100, # $1 in pennies
+      :description => 'One year of Bourn membership',
+      :currency    => 'usd'
+    )
+  end
+
+  def source_params
+    {
+      source: {
+        object: 'card',
+        exp_month: @form.exp_month,
+        exp_year: @form.exp_year,
+        number: @form.number,
+        cvc: @form.cvc,
+      }
+    }
   end
 end
---
+++ app/forms/subscription_form.rb
@@ -0,0 +1,7 @@
+class SubscriptionForm
+  include ActiveModel::Model
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  attr_accessor :number, :exp_month, :exp_year, :cvc, :postal_code
+end
